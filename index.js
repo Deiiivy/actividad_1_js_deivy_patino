ec243cecf82588ffa8cdb2f58314f049a4efbf79
Deiiivy <deivypr28@gmail.com>
2023-08-01 19:31:47 -0500

    claseee6

---
+++ index.js
@@ -0,0 +1,8 @@
+let name = 'Deivy'
+let lastName = 'Patiño'
+
+let birthday = 17
+
+let city = 'medellin'
+
+console.log(`Hola mi nombre es: ${name} ${lastName}, vivo en ${city} y tengo ${birthday} años ` )
